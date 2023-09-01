b02dff775b9539d48e757f3c82da9ab22f223759
NattyNgrn <natnrgn@gmail.com>
2023-09-01 00:24:23 -0400

    current weather pt2

--- client/src/components/weather.jsx
+++ client/src/components/weather.jsx
@@ -2,7 +2,8 @@ import temp from "../components/temp"
 import previous from "../components/previousCities"
 import hourly from "../components/hourly"
 import humidity from "../components/humidity"
-
+import sunrise from "../components/sunrise"
+import sunset from "../components/sunset"
 
 //city from use state in search cities.
 
@@ -10,14 +11,28 @@ function CurrentWeather() {
 
 return (
     <div className="weather">
-        
-         <h1>CITY</h1>
-        <h1>Previously searched cities:</h1>
-        <p>{previous}</p>
-        <h2>Hourly Forecast</h2>
-        <p>{hourly}</p>
-        <h2>Humidity</h2>
-        <p>{humidity}</p>
+        <h1>CITY</h1>
+
+        <div className="top">
+            <h2>Temperature</h2>
+            <p>{temp}</p>
+            <h2>Previously searched cities:</h2>
+            <p>{previous}</p>
+        </div>
+
+        <div className="middle">
+            <h2>Hourly Forecast</h2>
+            <p>{hourly}</p>
+        </div>
+
+        <div className="bottom">
+            <h2>Humidity</h2>
+            <p>{humidity}</p>
+            <h2>Sunrise</h2>
+            <p>{sunrise}</p>
+            <h2>Sunset</h2>
+            <p>{sunset}</p>
+        </div>
     </div>
 )
 }
