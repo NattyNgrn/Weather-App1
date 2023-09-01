36a48ee29e91dfa2ab3c666e5431feb563316609
NattyNgrn <natnrgn@gmail.com>
2023-09-01 13:46:53 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const port = process.env.PORT || 5555;
 import cors from "cors"; 
 import path from "path";
 
-const __dirname = path.resolve();
+const __dirname = "/Users/natalianegron/Documents/Techtonica/coolWeatherApp"
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/app.jsx")
+    res.sendFile(__dirname + "/client/src/app.jsx")
 });
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
