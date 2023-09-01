2d2d63473a8796b9d5229f0d8f7217ada657b845
NattyNgrn <natnrgn@gmail.com>
2023-08-31 23:05:32 -0400

    form building

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Form from "./components/form"
+
+const API = "1ed1fa56d4b93f26d1176e875b4c26fe";
+
+const Link = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>
-
+      <div className='container1'>
+        <Form onSearchChange={handleOnSearch}/>
       </div>
   )
 }
--- client/client/src/components/form.jsx
+++ client/client/src/components/form.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
+
+
+
 function Form() {
+    
+    const search = ({onSearchChange}) => {
+        const [search, setsearch] = useState("");
+        const handleOnChange = (searchData) => {
+            setsearch(searchData)
+            onSearchChange(searchData)
+        }
+    }
+
     return (
         <div>
             <h1>Search your city!</h1>
             <form>
-                <input type="text"></input>
+                <input type="text" value={search} onChange={handleOnChange}></input>
                 <button type="submit">Submit</button>
             </form>
         </div>
