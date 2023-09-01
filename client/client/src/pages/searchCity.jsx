f766bd1dcc57fd3ac7f2018d76cc8a660181afd8
NattyNgrn <natnrgn@gmail.com>
2023-08-31 21:41:52 -0400

    set up pt2

---
+++ client/client/src/pages/searchCity.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../style/searchCity.css'
+
+function App() {
+const [count, setCount] = useState(0)
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default App
