0281721aaf583de9e7fa8f4603c348ed444ceb57
NattyNgrn <natnrgn@gmail.com>
2023-08-31 21:53:45 -0400

    set up pt3

---
+++ client/client/src/components/form.jsx
@@ -0,0 +1,13 @@
+function Form() {
+    return (
+        <div>
+            <h1>Search your city!</h1>
+            <form>
+                <input type="text"></input>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
